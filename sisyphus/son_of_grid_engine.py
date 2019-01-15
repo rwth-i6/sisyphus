b7cb4c6e1da7c8d8c1efa459e28135d5674ee8d4
Jan-Thorsten Peter <peter@cs.rwth-aachen.de>
2019-01-15 09:03:28 +0100

    Added docu to SGE

--- sisyphus/son_of_grid_engine.py
+++ sisyphus/son_of_grid_engine.py
@@ -50,6 +50,7 @@ class SonOfGridEngine(EngineBase):
         :param bool auto_clean_eqw: if True jobs in eqw will be set back to qw automatically
         :param list[str] ignore_jobs: list of job ids that will be ignored during status updates.
                                       Useful if a job is stuck inside of SGE and can not be deleted.
+                                      Job should be listed as "job_number.task_id" e.g.: ['123.1', '123.2', '125.1']
         """
         self._task_info_cache_last_update = 0
         self.gateway = gateway
