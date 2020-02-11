a513ef1796641f0086932f78407c1cdae14212d5
Jan-Thorsten Peter <peter@cs.rwth-aachen.de>
2020-02-11 10:38:07 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sisyphus',
+    version='1.0.0',
+    packages=['sisyphus'],
+    license='Mozilla Public License Version 2.0',
+    long_description=open('README.md').read(),
+    author="Jan-Thorsten Peter",
+    author_email="jtpeter@apptek.com",
+    url="https://github.com/rwth-i6/sisyphus"
+)
