01bc62ffb50d8e55852fc148eeb436e6d3d966e2
Jan-Thorsten Peter <peter@cs.rwth-aachen.de>
2022-06-08 21:50:17 +0200

    Job inputs that are not stored locally are now also check for required inputs

--- sisyphus/job.py
+++ sisyphus/job.py
@@ -224,7 +224,7 @@ class Job(metaclass=JobSingleton):
         self._sis_inputs = set()
         self.__init__(*args, **kwargs)
 
-        self._sis_inputs.update({p for p in tools.extract_paths(self.__dict__) if p.creator != self})
+        self._sis_inputs.update({p for p in tools.extract_paths([self.__dict__, args, kwargs]) if p.creator != self})
 
         for i in self._sis_inputs:
             i.add_user(self)
