450ed58ec6ec964db0b8377b2b685d37b77fa3db
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2023-11-21 14:38:01 +0100

    No job cleanup for symlinked job dirs (#154)
    Fix #105

--- sisyphus/job.py
+++ sisyphus/job.py
@@ -497,6 +497,9 @@ class Job(metaclass=JobSingleton):
         elif self._sis_cleanable_cache:
             return True
         else:
+            if os.path.islink(self._sis_path()):
+                self._sis_cleaned_or_not_cleanable = True
+                return False
             cleanable = not os.path.isfile(self._sis_path(gs.JOB_FINISHED_ARCHIVE)) and self._sis_finished()
             if cleanable:
                 self._sis_cleanable_cache = True
