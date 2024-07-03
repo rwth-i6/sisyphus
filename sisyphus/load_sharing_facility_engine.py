bc4b1e79e3f292825e1af5a03f9cacec35687293
Nahuel Unai Roselló Beneitez <nbeneitez@apptek.com>
2024-07-03 11:23:08 -0400

    Black

--- sisyphus/load_sharing_facility_engine.py
+++ sisyphus/load_sharing_facility_engine.py
@@ -58,7 +58,7 @@ class LoadSharingFacilityEngine(EngineBase):
         logging.debug("shell_cmd: %s" % " ".join(system_command))
         if send_to_stdin:
             send_to_stdin = send_to_stdin.encode()
-        
+
         p = subprocess.run(system_command, input=send_to_stdin, capture_output=True, timeout=30)
 
         def fix_output(o):
