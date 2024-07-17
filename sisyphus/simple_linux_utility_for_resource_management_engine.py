6a7030072ef0a6b5dddb46cf41356685dbba7bfb
Nahuel Unai Roselló Beneitez <nbeneitez@apptek.com>
2024-07-17 07:04:45 -0400

    Handle retval != 0
    Whenever the return value is different from zero, the job state is eventually set as UNKNOWN. If the job is set as UNKNOWN and there's no log file attached, the job will be automatically queued. This is very dangerous when there's a job already queued "naturally", that is, queued because the queue scan job finished gracefully, found nothing, and set the job state as UNKNOWN.
    
    This wrong process can happen many times, until the first job scheduled eventually enters RUNNING state, and thus a log file is generated, preventing any further same job schedulings.

--- sisyphus/simple_linux_utility_for_resource_management_engine.py
+++ sisyphus/simple_linux_utility_for_resource_management_engine.py
@@ -77,6 +77,11 @@ class SimpleLinuxUtilityForResourceManagementEngine(EngineBase):
             return f"SSH command timeout: {command!s}"
         return f"Command timeout: {command!s}"
 
+    def _system_call_error_warn_msg(self, command: Any) -> str:
+        if self.gateway:
+            return f"SSH command error: {command!s}"
+        return f"Command error: {command!s}"
+
     def system_call(self, command, send_to_stdin=None):
         """
         :param list[str] command: qsub command
@@ -235,6 +240,10 @@ class SimpleLinuxUtilityForResourceManagementEngine(EngineBase):
         while True:
             try:
                 out, err, retval = self.system_call(sbatch_call)
+                if retval != 0:
+                    logging.warning(self._system_call_error_warn_msg(sbatch_call))
+                    time.sleep(gs.WAIT_PERIOD_BETWEEN_CHECKS)
+                    continue
             except subprocess.TimeoutExpired:
                 logging.warning(self._system_call_timeout_warn_msg(sbatch_call))
                 time.sleep(gs.WAIT_PERIOD_SSH_TIMEOUT)
@@ -307,6 +316,10 @@ class SimpleLinuxUtilityForResourceManagementEngine(EngineBase):
         while True:
             try:
                 out, err, retval = self.system_call(system_command)
+                if retval != 0:
+                    logging.warning(self._system_call_error_warn_msg(sbatch_call))
+                    time.sleep(gs.WAIT_PERIOD_BETWEEN_CHECKS)
+                    continue
             except subprocess.TimeoutExpired:
                 logging.warning(self._system_call_timeout_warn_msg(system_command))
                 time.sleep(gs.WAIT_PERIOD_SSH_TIMEOUT)
--- sisyphus/son_of_grid_engine.py
+++ sisyphus/son_of_grid_engine.py
@@ -79,6 +79,11 @@ class SonOfGridEngine(EngineBase):
             return f"SSH command timeout: {command!s}"
         return f"Command timeout: {command!s}"
 
+    def _system_call_error_warn_msg(self, command: Any) -> str:
+        if self.gateway:
+            return f"SSH command error: {command!s}"
+        return f"Command error: {command!s}"
+
     def system_call(self, command, send_to_stdin=None):
         """
         :param list[str] command: qsub command
@@ -251,6 +256,10 @@ class SonOfGridEngine(EngineBase):
         while True:
             try:
                 out, err, retval = self.system_call(qsub_call, command)
+                if retval != 0:
+                    logging.warning(self._system_call_error_warn_msg(qsub_call))
+                    time.sleep(gs.WAIT_PERIOD_BETWEEN_CHECKS)
+                    continue
             except subprocess.TimeoutExpired:
                 logging.warning(self._system_call_timeout_warn_msg(qsub_call))
                 time.sleep(gs.WAIT_PERIOD_SSH_TIMEOUT)
@@ -318,6 +327,10 @@ class SonOfGridEngine(EngineBase):
         while True:
             try:
                 out, err, retval = self.system_call(system_command)
+                if retval != 0:
+                    logging.warning(self._system_call_error_warn_msg(system_command))
+                    time.sleep(gs.WAIT_PERIOD_BETWEEN_CHECKS)
+                    continue
             except subprocess.TimeoutExpired:
                 logging.warning(self._system_call_timeout_warn_msg(system_command))
                 time.sleep(gs.WAIT_PERIOD_SSH_TIMEOUT)
