da7f29ce8b07987210ce08db8d316b5dee8c7d8c
Nahuel Unai Roselló Beneitez <31628502+Icemole@users.noreply.github.com>
2024-08-26 13:47:37 +0200

    Queue scan: handle return value different from zero (#198)

--- sisyphus/load_sharing_facility_engine.py
+++ sisyphus/load_sharing_facility_engine.py
@@ -48,6 +48,11 @@ class LoadSharingFacilityEngine(EngineBase):
             return f"SSH command timeout: {command!s}"
         return f"Command timeout: {command!s}"
 
+    def _system_call_error_warn_msg(self, command: Any) -> str:
+        if self.gateway:
+            return f"SSH command error: {command!s}"
+        return f"Command error: {command!s}"
+
     def system_call(self, command, send_to_stdin=None):
         if self.gateway:
             system_command = ["ssh", "-x", self.gateway] + [" ".join(["cd", os.getcwd(), "&&"] + command)]
@@ -249,6 +254,10 @@ class LoadSharingFacilityEngine(EngineBase):
         while True:
             try:
                 out, err, retval = self.system_call(system_command)
+                if retval != 0:
+                    logging.warning(self._system_call_error_warn_msg(system_command))
+                    time.sleep(gs.WAIT_PERIOD_QSTAT_PARSING)
+                    continue
             except subprocess.TimeoutExpired:
                 logging.warning(self._system_call_timeout_warn_msg(system_command))
                 time.sleep(gs.WAIT_PERIOD_SSH_TIMEOUT)
