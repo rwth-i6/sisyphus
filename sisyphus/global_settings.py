9578b6f233e5fd3301927005cf61c260e0381894
Raphael Reinauer <134445357+RaphaelLilt@users.noreply.github.com>
2024-10-24 14:26:02 +0200

    Set INCLUDE_CREATOR_STATE to False

--- sisyphus/global_settings.py
+++ sisyphus/global_settings.py
@@ -206,7 +206,7 @@ ALIAS_AND_OUTPUT_SUBDIR = ""
 SHOW_JOB_TARGETS = True
 
 # Controls whether to include 'creator' in serialized state of an abstract path
-INCLUDE_CREATOR_STATE = True
+INCLUDE_CREATOR_STATE = False
 
 #: How many seconds should be waited before assuming a job is finished after the finished file is written
 #: to allow network file system to sync up
