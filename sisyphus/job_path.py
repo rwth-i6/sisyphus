e505f7868aebd876c7503f3aabe2c27442d0c2af
RaphaelLilt <raphael.reinauer@lilt.com>
2024-10-25 18:02:04 +0200

    Black cleanup

--- sisyphus/job_path.py
+++ sisyphus/job_path.py
@@ -250,11 +250,10 @@ class AbstractPath(DelayedBase):
         result = cls.__new__(cls)
         memo[id(self)] = result
         for k, v in self.__dict__.items():
-            if k != 'users':
+            if k != "users":
                 setattr(result, k, copy.deepcopy(v, memo))
         result.users = set()
         return result
-
 
     def __getstate__(self):
         """Skips exporting users
