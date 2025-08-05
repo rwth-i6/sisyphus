d7044d70614f3b6ddd18a1569754f7df30318091
Moritz Gunz <moritz.gunz@gmail.com>
2025-08-05 12:38:42 +0200

    drop runtime interface in favor of simply using `gs.active_engine`

--- sisyphus/__init__.py
+++ sisyphus/__init__.py
@@ -2,14 +2,13 @@ import sys
 from .job import Job
 from .task import Task
 import sisyphus.toolkit as tk
-import sisyphus.runtime as rt
 from sisyphus.loader import RecipeFinder
 
 # setup_path and gs will be removed in the future since they are both accessible via tk
 gs = tk.gs
 setup_path = tk.setup_path
 Path = tk.Path
-__all__ = ["Job", "Task", "Path", "gs", "tk", "setup_path", "rt"]
+__all__ = ["Job", "Task", "Path", "gs", "tk", "setup_path"]
 
 
 # Setup recipe importer
