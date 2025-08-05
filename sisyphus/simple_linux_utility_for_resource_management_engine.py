9012c9eacc263e5a4cc1097dc7623137a8c73860
Moritz Gunz <moritz.gunz@gmail.com>
2025-08-05 12:49:45 +0200

    formatting

--- sisyphus/simple_linux_utility_for_resource_management_engine.py
+++ sisyphus/simple_linux_utility_for_resource_management_engine.py
@@ -463,14 +463,10 @@ class SimpleLinuxUtilityForResourceManagementEngine(EngineBase):
         """
         env_vars = ["SLURM_JOB_NODELIST", "SLURM_NODELIST"]
         env_var = next((v for v in env_vars if v in os.environ), None)
-        assert env_var is not None, (
-            f"neither of {'/'.join(env_vars)} is set, are we running in a worker context?"
-        )
+        assert env_var is not None, f"neither of {'/'.join(env_vars)} is set, are we running in a worker context?"
         partaking_nodes = os.environ[env_var]
         nodes = sorted(
             node_name.strip()
-            for node_name in subprocess.check_output(
-                ["scontrol", "show", "hostnames", partaking_nodes], text=True
-            )
+            for node_name in subprocess.check_output(["scontrol", "show", "hostnames", partaking_nodes], text=True)
         )
         return nodes
